36f201e9d3a2f08eaebc780f2a64ee496211ab85
Scott DeBoer <mtbfanatic1@gmail.com>
2018-02-01 13:32:47 -0600

    Removed files and started over

--- app.js
+++ app.js
@@ -4,20 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var expressHbs = require('express-handlebars');
-var mongoose = require('mongoose');
+
 var index = require('./routes/index');
-var tickets = require('./routes/tickets');
-var nodemailer = require("nodemailer");
+var users = require('./routes/users');
 
 var app = express();
 
-// mongoose.connect('127.0.0.1:27017/nodeapp');
-
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
